41c53f6be5df0b26f588be623cd455cf83e1fe47
Alex <Alwong@ya.ru>
2023-03-09 22:58:05 +0400

    update

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
 const eyeLeft = document.getElementById('pupil-left');
 const eyeRight = document.getElementById('pupil-right');
 
-function slideLeft() {
+function moveLeft() { 
     eyeLeft.style.marginLeft = `-20px`;    
     eyeRight.style.marginLeft = `-20px`;    
 }
-function slideRight() {
+function moveRight() {
     eyeLeft.style.marginLeft = `20px`;    
     eyeRight.style.marginLeft = `20px`;    
 }
-function slideHome() {
+function moveHome() {
     eyeLeft.style.marginLeft = `0`;    
     eyeRight.style.marginLeft = `0`;    
 }
+function moveDown() {
+    eyeLeft.style.marginTop = `6px`;    
+    eyeRight.style.marginTop = `6px`;     
+}
+function moveUp() {
+    eyeLeft.style.marginTop = `0px`;    
+    eyeRight.style.marginTop = `0px`;     
+}
 
+function setTransition(size) {
+    eyeLeft.style.transition = `${size}s`; 
+    eyeRight.style.transition = `${size}s`;     
+}
 
-function shake() {
-    let delay = 1500;
-    slideLeft();
+function shake() {  
+    setTransition(0.4);   
+    moveLeft();
+    moveDown();
     setTimeout(() => {
-        slideRight();
+        setTransition(1.5);        
+        moveRight();
         setTimeout(() => {
-            slideLeft();
+            setTransition(0.4); 
+            moveLeft();
             setTimeout(() => {
-                slideRight();
+                setTransition(1.5);                 
+                moveRight();
                 setTimeout(() => {
-                    slideHome();
-                }, delay)                 
-            }, delay)             
-        }, delay)          
-    }, delay)    
+                    setTransition(0.4);                    
+                    moveHome();
+                    moveUp(); 
+                }, 1500)                 
+            }, 500)             
+        }, 1500)          
+    }, 500)
+   
 }
 
 
